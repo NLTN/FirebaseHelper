4dd709fbc6611b52315eb5d4d8248743b82bb84c
Nguyen Nguyen <nguyennguyen@Nguyens-MacBook.local>
2016-12-28 21:09:26 -0800

    Initial Commit

---
+++ FirebaseHelper/FirebaseHelper.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseHelper.h
+//  FirebaseHelper
+//
+//  Created by Nguyen Nguyen on 12/28/16.
+//  Copyright © 2016 Nguyen Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseHelper.
+FOUNDATION_EXPORT double FirebaseHelperVersionNumber;
+
+//! Project version string for FirebaseHelper.
+FOUNDATION_EXPORT const unsigned char FirebaseHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseHelper/PublicHeader.h>
+
+
